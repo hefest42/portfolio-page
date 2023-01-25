f44a33e81a80c4307732c253867fa4706dbe35c1
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-01-25 16:05:43 +0100

    first pass on skills section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SkillsSection from "./components/SkillsSection";
 
 function App() {
     return (
         <div className="bg-slate-800 text-white">
             <Header />
             <Hero />
+            <SkillsSection />
         </div>
     );
 }
---
+++ src/components/SkillsSection.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SkillsSection = () => {
+    return (
+        <section className="p-16 border-y-2 flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center items-center">
+                <div className="text-3xl p-6">HTML</div>
+                <div className="text-3xl p-6">CSS</div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <div className="text-3xl p-6">JavaScript</div>
+                <div className="text-3xl p-6">React</div>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+                <div className="text-3xl p-6">Redux</div>
+                <div className="text-3xl p-6">Sass</div>
+            </div>
+        </section>
+    );
+};
+
+export default SkillsSection;

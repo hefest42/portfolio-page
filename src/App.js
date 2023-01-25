3e34eae478e3dd81c0c548b5299e999e396c7d47
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-01-25 16:47:34 +0100

    added contact form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import SkillsSection from "./components/SkillsSection";
+import Contact from "./components/Contact";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Header />
             <Hero />
             <SkillsSection />
+            <Contact />
         </div>
     );
 }

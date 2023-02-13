8a226ecbb1e1fb85e67fcc1d3ab0c51ce0d78a40
Nemanja Vidovic <nemanjavidovic93@gmail.com>
2023-02-13 15:36:10 +0100

    redesigned contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,8 +8,8 @@ const Contact = ({ contactRef }) => {
     };
 
     return (
-        <div className=" w-11/12 border-b-4 flex flex-col justify-center items-center" ref={contactRef}>
-            <div className="flex flex-col justify-center items-center">
+        <div className="w-11/12 border-b-2 flex flex-col justify-center items-center" ref={contactRef}>
+            <div className="md:w-1/2 flex flex-col justify-center items-center">
                 <div className="flex flex-col justify-start items-center">
                     <h1 className="text-5xl">Contact</h1>
                     <p className="text-lg text-center mt-8 text-slate-300">
@@ -19,22 +19,22 @@ const Contact = ({ contactRef }) => {
                 </div>
 
                 <form className="w-full py-12" onSubmit={sendEmailHandler}>
-                    <Input type="text" name="NAME" />
-                    <Input type="email" name="EMAIL" />
+                    <Input type="text" name="Name" />
+                    <Input type="email" name="Email" />
 
-                    <div className="mt-4">
-                        <label className="text-slate-400" htmlFor="text-area">
-                            MESSAGE
-                        </label>
+                    <div className="mt-4 h-48 bg-slate-700 rounded-3xl flex flex-col justify-start items-start">
                         <textarea
                             required="yes"
-                            className="w-full h-[6rem] bg-transparent text-xl mt-4 border-b-[2px] border-slate-400 outline-none"
+                            className="w-full bg-transparent text-xl mt-4 px-4 outline-none"
                             name="text-area"
                             id="text-area"
-                            cols="30"
+                            cols="40"
                             rows="10"
-                        ></textarea>
+                            placeholder="Message"
+                        />
                     </div>
+
+                    <button className="bg-indigo-500 w-full h-10 rounded-3xl mt-4">Send</button>
                 </form>
             </div>
         </div>
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,17 +1,25 @@
 import React from "react";
 
+import { FaUserAlt } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+
 const Input = ({ type, name }) => {
     return (
-        <div className="flex flex-col">
-            <label className="mt-4 text-slate-400" htmlFor={name}>
-                {name}
+        <div className="bg-slate-700 w-full h-10 rounded-3xl flex justify-start items-center mt-4">
+            <label className="text-slate-400 ml-4 flex justify-start items-center" htmlFor={name}>
+                {name === "Name" ? (
+                    <FaUserAlt className="w-[20px] h-[20px]" />
+                ) : (
+                    <MdEmail className="w-[20px] h-[20px]" />
+                )}
             </label>
             <input
-                className="bg-transparent border-b-[2px] border-slate-400 outline-none py-2 "
+                className="flex-1 bg-transparent outline-none ml-2 text-lg"
                 type={type}
                 id={name}
                 name={name}
                 required="yes"
+                placeholder={name}
             />
         </div>
     );
